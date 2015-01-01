d51ed17d623f9d569a52e74621a0dbd6aff3dcd0
tasman666 <a.trzepacz@gmail.com>
2015-01-01 20:27:19 +0100

    implementation for parallel filtering

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -4,7 +4,7 @@ import java.util.concurrent._
 
 object Par {
   type Par[A] = ExecutorService => Future[A]
-  
+
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
@@ -12,15 +12,20 @@ object Par {
   def lazyUnit[A](a: A): Par[A] = fork(unit(a))
 
   private case class UnitFuture[A](get: A) extends Future[A] {
-    def isDone = true 
-    def get(timeout: Long, units: TimeUnit) = get 
-    def isCancelled = false 
-    def cancel(evenIfRunning: Boolean): Boolean = false 
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit) = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
-  private case class FutureWithTimeout[A,B,C](f1: Future[A], f2: Future[B])(f: (A,B) => C) extends Future[C] {
+  private case class FutureWithTimeout[A, B, C](f1: Future[A], f2: Future[B])(f: (A, B) => C) extends Future[C] {
     var cache: Option[C] = None
+
     def isDone = cache.isDefined
+
     def get(timeout: Long, units: TimeUnit) = cache match {
       case Some(c) => c
       case None =>
@@ -31,24 +36,27 @@ object Par {
         cache = Some(f(a, b))
         cache.get
     }
+
     def get() = cache match {
       case Some(c) => c
       case None =>
-          cache = Some(f(f1.get(), f2.get()))
-          cache.get
+        cache = Some(f(f1.get(), f2.get()))
+        cache.get
     }
+
     def isCancelled = f1.isCancelled || f2.isCancelled
+
     def cancel(evenIfRunning: Boolean): Boolean = f1.cancel(evenIfRunning) || f2.cancel(evenIfRunning)
   }
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
     (es: ExecutorService) => {
-      val af = a(es) 
+      val af = a(es)
       val bf = b(es)
       UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
     }
 
-  def map2Timeout[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+  def map2Timeout[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
     (es: ExecutorService) => {
       val af = a(es)
       val bf = b(es)
@@ -56,23 +64,23 @@ object Par {
     }
 
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
-    es => es.submit(new Callable[A] { 
+    es => es.submit(new Callable[A] {
       def call = a(es).get
     })
 
-  def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
-    map2(pa, unit(()))((a,_) => f(a))
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit(()))((a, _) => f(a))
 
   def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
 
-  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = 
+  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
     p(e).get == p2(e).get
 
-  def delay[A](fa: => Par[A]): Par[A] = 
+  def delay[A](fa: => Par[A]): Par[A] =
     es => fa(es)
 
   def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
-    es => 
+    es =>
       if (run(es)(cond).get) t(es) // Notice we are blocking on the result of `cond`.
       else f(es)
 
@@ -84,7 +92,7 @@ object Par {
 
   }
 
-  def asyncF[A,B](f: A => B): A => Par[B] = {
+  def asyncF[A, B](f: A => B): A => Par[B] = {
     a => lazyUnit(f(a))
   }
 
@@ -95,11 +103,16 @@ object Par {
     }
 
   def sequence_simple[A](l: List[Par[A]]): Par[List[A]] =
-    l.foldRight[Par[List[A]]](unit(List()))((h,t) => map2(h,t)(_ :: _))
+    l.foldRight[Par[List[A]]](unit(List()))((h, t) => map2(h, t)(_ :: _))
 
-  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork {
     val fbs: List[Par[B]] = ps.map(asyncF(f))
     sequence_simple(fbs)
+  }
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] = as map (asyncF((a: A) => if (f(a)) List(a) else List()))
+    map(sequence(pars))(_.flatten)
   }
 }
 
@@ -121,10 +134,12 @@ object Examples {
       Par.map2Timeout(Par.fork(sumPar(l)), Par.fork(sumPar(r)))(_ + _)
     }
 
+
   def main(args: Array[String]): Unit = {
     val executor: ExecutorService = Executors.newFixedThreadPool(10)
     calculateSum(executor)
     calculateProduct(executor)
+    filter(executor)
     executor.shutdown()
   }
 
@@ -138,6 +153,12 @@ object Examples {
     val parProduct = Par.parMap(List(3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,13,14,14))(a => a * 3)
     val result = Par.run(executor)(parProduct)
     println("product result:" + result.get(4, TimeUnit.SECONDS))
+  }
+
+  def filter(executor: ExecutorService) = {
+    val parFilter = Par.parFilter(List(3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,13,14,14))(a => a % 2 == 0)
+    val result = Par.run(executor)(parFilter)
+    println("filter result:" + result.get(4, TimeUnit.SECONDS))
   }
 
 }
